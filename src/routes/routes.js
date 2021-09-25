f27e8cee807fc4514b5afa1b40ac9a5b0beafb4b
miguelgc95 <miguelgc95@gmail.com>
2021-09-25 11:17:24 +0200

    routing

---
+++ src/routes/routes.js
@@ -0,0 +1,11 @@
+export const HOME = '/';
+
+export const HOME_USER = '/user/:username';
+export const SIGN_UP_USER = '/user/sign-up';
+export const LOGIN_USER = '/user/login';
+
+export const HOME_BUSINESS_USER = '/business/:businessname';
+export const SIGN_UP_BUSINESS_USER = '/business/sign-up';
+export const LOGIN_BUSINESS_USER = '/business/login';
+
+export const SEARCH = '/search';
